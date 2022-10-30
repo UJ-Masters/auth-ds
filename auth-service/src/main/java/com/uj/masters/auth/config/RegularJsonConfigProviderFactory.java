d64be685f17c5fda5790df5d8c6fd280a6eb87b1
mkay47 <201402344@student.uj.ac.za>
2022-10-30 17:05:57 +0200

    project init

---
+++ auth-service/src/main/java/com/uj/masters/auth/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.uj.masters.auth.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
